992e488b16c28b1a867f7d54b8d03a263fb8f6b0
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-22 13:18:23 -0300

    aula type annotation ts adicionado

---
+++ src/A0001-type-annotation/type-annotation.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// Tipos básicos ( aqui ocorre a inferência de tipos)
+
+let nome: string = 'Luiz'; // Qualquer tipo de string: '' "" ``
+
+let idade: number = 0b1010; // 10,  1.57, 3.14, -5.55, 0xf00d, 0b1010
+
+let adulto: boolean = true;
+
+let simbolo: symbol = Symbol('qualquer-symbol');
+
+// let big: bigint = 10n; //bigint
+
+// Arrays
+let arrayOfNumbers : Array<number> = [2,4,6,8];
+let arrayOfNumbers2 : number[] = [2,4,6,8];
+
+let arrayOfStrings: Array<string> = ['aaa', 'axf', 'g'];
+let arrayOfStrings2: string[] = ['aaa', 'axf', 'g'];
+
+// Objetos
+let person: {name: string, age: number, adult?: boolean} = {
+  name: 'Vitor',
+  age: 20
+};
+
+
+// Funções
+function soma(x: number, y:number){
+  return x + y;
+}
+
+const result = soma(2,3)
+console.log(result)
