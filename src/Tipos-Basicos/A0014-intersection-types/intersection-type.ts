daeff935e25e98005f285cd529303f654e09e6ee
aSTRonunn <vitormanoel00710@gmail.com>
2021-07-28 12:45:23 -0300

    arquivos type-ts adicionados

---
+++ src/Tipos-Basicos/A0014-intersection-types/intersection-type.ts
@@ -0,0 +1,16 @@
+type TemNome = { nome: string };
+type TemSobrenome = { sobrenome: string };
+type TemIdade = { idade: number };
+type Pessoa = TemNome & TemSobrenome & TemIdade;
+
+type AB = 'A' | 'B';
+type AC = 'A' | 'C';
+type Intersection = AB & AC;
+
+const pessoa: Pessoa = {
+  nome: 'Luiz',
+  sobrenome: 'Miranda',
+  idade: 30,
+};
+
+console.log(pessoa);
