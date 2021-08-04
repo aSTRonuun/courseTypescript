44f174bdf319d0447f5ff53dbc87aedd6ddc58f4
aSTRonunn <vitormanoel00710@gmail.com>
2021-08-04 13:29:43 -0300

    arquivos poo-ts adicionados

---
+++ src/02-POO/A0030-association/association.ts
@@ -0,0 +1,61 @@
+export class Escritor {
+  // Inversãoo de Dependência onde a classe escritor usar uma classe abstrata que é o contrato
+  // É bem melhor, pois, geralmente o contrato (classe abstrata) é bem menos modificada do que é as subclasse da classe abstrata
+  private _ferramenta: Ferramenta | null = null;
+
+  constructor(private _nome: string) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this.ferramenta === null) {
+      console.log('Não posso escrever sem ferramenta...');
+    } else {
+      this.ferramenta.escrever();
+    }
+  }
+}
+
+export abstract class Ferramenta {
+  constructor(private _nome: string) {}
+  abstract escrever(): void;
+
+  get nome(): string {
+    return this._nome;
+  }
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está escrevendo`);
+  }
+}
+
+export class MaquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Machado de Assis');
+const caneta = new Caneta('Bic');
+const maquinaEscrever = new MaquinaEscrever('Máquina');
+
+// console.log(escritor.nome);
+// console.log(caneta.nome);
+// console.log(maquinaEscrever.nome);
+
+escritor.ferramenta = caneta;
+escritor.ferramenta = maquinaEscrever;
+escritor.ferramenta = null;
+escritor.escrever();
